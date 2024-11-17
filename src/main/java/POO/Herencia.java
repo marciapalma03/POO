d3785eb4a9f1c05d686a7d457a9c7efbd5629d6e
Marcia <mapalma@uth.hn>
2024-11-16 23:23:54 -0600

    Actualización Clase 2024-11-16

--- src/main/java/POO/Herencia.java
+++ src/main/java/POO/Herencia.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.herencia;
+package POO;
 
 import java.util.ArrayList;
 import java.util.List;
